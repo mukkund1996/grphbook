844cfb5ec9250654a42e0df6ec271e158da2df36
Mukkund Sunjii <m.babugheethaa@shell.com>
2023-07-02 10:26:17 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import { Notebook, Jupyter } from "@datalayer/jupyter-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Jupyter>
+      <Notebook path="ping.ipynb" />
+    </Jupyter>
+  )
 }
 
 export default App;
