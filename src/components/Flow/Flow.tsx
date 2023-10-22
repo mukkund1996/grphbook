68d7d15fb83b243a3b3e9d431ea512e688ecd069
Mukkund Sunjii <m.babugheethaa@shell.com>
2023-10-22 18:26:16 +0200

    added text input

--- src/components/Flow/Flow.tsx
+++ src/components/Flow/Flow.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useState} from "react";
+import { useCallback, useState } from "react";
 import {
   Connection,
   Edge,
@@ -18,23 +18,30 @@ import "reactflow/dist/style.css";
 import CodingNode from "../CodingNode/CodingNode";
 import DescriptionNode from "../DescriptionNode/DescriptionNode";
 import CustomControl from "../Controls/Controls";
-import {INITIAL_EDGES, INITIAL_NODES} from "../../state/InitialState";
+import { INITIAL_EDGES, INITIAL_NODES } from "../../state/InitialState";
+import { GeneratorInput } from "../Generator/GeneratorInput";
 
-const nodeTypes: NodeTypes = {codingCell: CodingNode, descriptionCell: DescriptionNode};
+const nodeTypes: NodeTypes = {
+  codingCell: CodingNode,
+  descriptionCell: DescriptionNode,
+};
 
 const Flow: React.FC = () => {
   const [nodes, setNodes] = useState<Array<Node>>(INITIAL_NODES);
   const [edges, setEdges] = useState<Array<Edge>>(INITIAL_EDGES);
 
   const onNodesChange: OnNodesChange = useCallback(
-    changes => setNodes(nds => applyNodeChanges(changes, nds)),
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
     [setNodes]
   );
   const onEdgesChange: OnEdgesChange = useCallback(
-    changes => setEdges(eds => applyEdgeChanges(changes, eds)),
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     [setEdges]
   );
-  const onConnect = useCallback((params: Edge | Connection) => setEdges(els => addEdge(params, els)), [setEdges]);
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((els) => addEdge(params, els)),
+    [setEdges]
+  );
 
   return (
     <ReactFlow
@@ -47,6 +54,7 @@ const Flow: React.FC = () => {
     >
       <MiniMap />
       <CustomControl />
+      <GeneratorInput />
     </ReactFlow>
   );
 };
--- src/components/Styles/common.styles.tsx
+++ src/components/Styles/common.styles.tsx
@@ -1,3 +1,11 @@
-export const buttonSx = {
+import { CSSProperties } from "react";
+
+export const buttonSx: CSSProperties = {
   border: "none",
+};
+
+export const borderStyles: CSSProperties = {
+  border: "3px solid #f4f4f4",
+  borderRadius: "5px",
+  background: "#fff",
 };
