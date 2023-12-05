bc77937b19730352fc97ef19d745e506a3f46dd8
Mukkund Sunjii <m.babugheethaa@shell.com>
2023-12-05 15:41:18 +0100

    added dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
